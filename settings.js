85ca8b41880c6a6d23b220eeb0a2a5adc6a0b3b6
RUKAMALLI0 <134293662+RUKAMALLI0@users.noreply.github.com>
2023-06-11 07:51:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,8 +16,8 @@ global.ONLY_GROUPS = false // Only Groups Use
 /*
 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
-global.OWNER_NAME = 'Dark Maker Ofc' // Owner Name
-global.OWNER_NUMBER = ['94715166712'] //Owner Number
+global.OWNER_NAME = 'M.R.Rukka' // Owner Name
+global.OWNER_NUMBER = ['94702287970'] //Owner Number
 global.BOT_ADMINS = ['94715166712','94715166712'] //Bot admins numbers
 global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
 
